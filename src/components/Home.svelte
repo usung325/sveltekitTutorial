5af8f5dd729bdd4be44942c680e7d150c6d6e7dc
usung325 <usung325@gmail.com>
2024-03-23 12:13:30 -0400

    init commit

---
+++ src/components/Home.svelte
@@ -0,0 +1,12 @@
+<script>
+    let quantity = 0;
+
+    function addToCart() {
+        quantity = ++quantity
+    };
+</script>
+
+
+<p class="m-4">hello my name is {quantity}</p>
+
+<button on:click={addToCart} class="m-4 p-2 bg-blue-500 rounded-md text-white">button</button>
