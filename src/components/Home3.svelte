dd112ce746693e5c8350beff0322ae7554dfad01
usung325 <usung325@gmail.com>
2024-04-06 23:01:09 -0400

    element directives

---
+++ src/components/Home3.svelte
@@ -0,0 +1,23 @@
+<script>
+    let searchedVal = '';
+
+    let checked = false;    
+
+
+</script>
+
+<div class='p-10'>
+    <h3>
+        Searched: {searchedVal}
+    </h3>
+    <input class="outline outline-1" bind:value={searchedVal} >
+
+    <br>
+    <br>
+    
+    <h3>
+        Checked: {checked}
+    </h3>
+    <input type="checkbox" bind:checked = {checked} >
+</div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script>
     import Home from "../components/Home.svelte";
     import Home2 from "../components/Home2.svelte";
+    import Home3 from "../components/Home3.svelte";
 
 
 </script>
 
 <!-- <Home /> -->
-<Home2 />
+<Home3 />
