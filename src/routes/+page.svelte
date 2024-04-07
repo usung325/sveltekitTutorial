330b8064a888446896ff0e04a1fa0b9c04be4235
usung325 <usung325@gmail.com>
2024-04-06 23:03:20 -0400

    setup Home4.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,10 @@
     import Home from "../components/Home.svelte";
     import Home2 from "../components/Home2.svelte";
     import Home3 from "../components/Home3.svelte";
+    import Home4 from "../components/Home4.svelte";
 
 
 </script>
 
 <!-- <Home /> -->
-<Home3 />
+<Home4 />
