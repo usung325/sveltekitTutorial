c5f56830ffea70a0fa3e74d3ec12a1a261a16d33
usung325 <usung325@gmail.com>
2024-04-06 22:44:38 -0400

    practice on:mousemove and on:click

--- src/components/Home2.svelte
+++ src/components/Home2.svelte
@@ -1,14 +1,17 @@
 <script>
-    let alert2 = 'lmao';
+
     function handleClick(string){
         alert(string);
     }
 </script>
 
-<button on:click={()=>{handleClick(alert2)}}>hi</button>
+<button on:click={()=>{handleClick('lol')}}>
+    CLick me!
+</button>
 
-<div class>
-    <div>
-
+<div on:mousemove = {()=> {handleClick('say')}} class="bg-green-500 p-10">
+    click
+    <div class="bg-red-500">
+        red
     </div>
 </div>
