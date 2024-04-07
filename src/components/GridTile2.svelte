66b20c4df348247de04ce6396e323ffc3ff87f60
usung325 <usung325@gmail.com>
2024-04-07 02:48:25 -0400

    send props from parent to child in +page.svelte to GridTile.svelte. super useful.

---
+++ src/components/GridTile2.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let product = {
+        name: 'no product',
+        price: 0,
+        src: '/favicon.png'
+    }
+    // let products = [
+    //     {name: 'cup', quantity: 10},
+    //     {name: 'spoon', quantity: 20},
+    //     {name: 'fork', quantity: 30},
+    //     {name: 'napkin', quantity: 500}
+    // ]
+</script>
+
+
+<div class="p-10 bg-purple-700 flex">
+    <div class="my-auto">
+        <div class='text-white bg-yellow-400 content-center px-6'>
+            {product.name}
+        </div>
+        <div class="bg-white text-center">
+            ${product.price} Mil
+        </div>
+    </div>
+    <div>
+        <img alt="The project logo" src={product.src} width=200 />
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,24 @@
 <script>
+    import GridTile2 from "../components/GridTile2.svelte";
     import Home from "../components/Home.svelte";
     import Home2 from "../components/Home2.svelte";
     import Home3 from "../components/Home3.svelte";
     import Home4 from "../components/Home4.svelte";
     import Home5 from "../components/Home5.svelte";
 
+    let products = [
+        {name: 'Eva01', src:'/images/temp.webp', price: 50},
+        {name: 'Eva02', src:'/images/temp2.webp', price: 40},
+        {name: 'Eva03', src:'/images/temp3.webp', price: 35}
+    ]
+
 
 </script>
 
 <!-- <Home /> -->
-<Home5 />
+<main class="">
+    {#each products as product (product.name)}
+        <GridTile2 product = {product} />
+    {/each}
+    <GridTile2 />
+</main>
